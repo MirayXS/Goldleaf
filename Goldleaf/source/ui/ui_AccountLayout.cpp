7e54a6636f362f4785fa597a4726b82aea295195
Ivan <miraybusiness01@gmail.com>
2023-09-10 01:03:12 +0200

    Update ui_AccountLayout.cpp

--- Goldleaf/source/ui/ui_AccountLayout.cpp
+++ Goldleaf/source/ui/ui_AccountLayout.cpp
@@ -3,7 +3,6 @@
 
     Goldleaf - Multipurpose homebrew tool for Nintendo Switch
     Copyright (C) 2018-2023 XorTroll
-
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
