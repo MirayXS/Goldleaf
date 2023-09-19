0a8eedd15df79a786fb8f0d027406908cdce7490
Ivan <miraybusiness01@gmail.com>
2023-09-19 20:17:56 +0200

    Update ui_AccountLayout.cpp

--- Goldleaf/source/ui/ui_AccountLayout.cpp
+++ Goldleaf/source/ui/ui_AccountLayout.cpp
@@ -79,9 +79,9 @@ namespace ui {
     }
 
     void AccountLayout::optsRename_DefaultKey() {
-        const auto name = ShowKeyboard(cfg::Strings.GetString(213), this->cur_prof_base.nickname, 10, sizeof(this->cur_prof_base.nickname) - 1);
+        const auto name = ShowKeyboard(cfg::Strings.GetString(213), "");
         if(!name.empty()) {
-            strcpy(this->cur_prof_base.nickname, "ancient hacking ritual ");
+            strcpy(this->cur_prof_base.nickname, name.c_str());
             const auto rc = acc::EditUser([&](AccountProfileBase *prof_base, AccountUserData *_user_data) {
                 memcpy(prof_base, &this->cur_prof_base, sizeof(this->cur_prof_base));
             });
